1aa9d4a00ffc0d50b35753453440ea895a987970
LOGAN HOOVER <lh0987936@otc.edu>
2025-09-05 12:43:29 -0500

    Finished!

--- slot_game.php
+++ slot_game.php
@@ -4,21 +4,24 @@ $winnings = 0;
 $payoff = 0;
 $spin_total = 1;
 $display = '';
+
 while ($winnings < 500 && $spin_total <= 10) {
     
     for ($i = 1; $i <= 3; $i++) {
         $result = array_rand($slots);
         $display .= $slots[$result];
     }
-    echo("spin " . $spin_total . "). " .$display);
+    echo("\nSpin " . $spin_total . ". " .$display);
     match ($display) {
         'AAA', 'BBB', 'CCC' => $payoff = 100,
-        'ABB', 'ABA', 'BAA', 'ABB', 'BBA', 'BAB', 'BCC', 'CBC', 'CCB', 'ACC', 'CAC', 'CCA' => $payoff = 50,
+        'AAB', 'ABA', 'BAA', 'ABB', 'BBA', 'BAB', 'BCC', 'CBC', 'CCB', 'ACC', 'CAC', 'CCA', 'CAA', 'AAC', 'ACA', 'CBB', 'BCB', 'BBC' => $payoff = 50,
         default => $payoff = 0,
     };
-    echo("\npayout: " . $payoff . "!");
+    echo("\npayout: " . $payoff . "!\n");
     $winnings += $payoff;
     $display = "";
-    echo("\nwinnings : " . $winnings . "!\n");
     $spin_total++;
 }
+echo("That's the Last Spin!\n");
+echo("\nTotal winnings : " . $winnings . "!\n");
+echo("Thanks for playing! Please, join us again soon!");
